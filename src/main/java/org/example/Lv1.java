7b074a641123a75203f08614fb85ef6dd1288fec
Soong0812 <shotpo90@gmail.com>
2025-07-16 10:44:26 +0900

    Lv.1

---
+++ src/main/java/org/example/Lv1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Lv1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        }
+    }
+}
