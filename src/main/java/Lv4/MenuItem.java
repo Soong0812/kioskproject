ab11d6ee8a1d4e267825b6f98ac27a7f190248b1
Soong0812 <shotpo90@gmail.com>
2025-07-21 17:41:40 +0900

    Lv.5

--- src/main/java/Lv4/MenuItem.java
+++ src/main/java/Lv4/MenuItem.java
@@ -1,4 +1,5 @@
 package Lv4;
+//Lv3 메뉴아이템 클래스 가져오기
 
 public class MenuItem {
     private final String name;
@@ -20,9 +21,13 @@ public class MenuItem {
     public String getExplain() {
         return explain;
     }
+    //getter 활용
 
     @Override
     public String toString() {
         return name + " " + price + " " + explain;
     }
+    //생각하지 못하고 해당 코드 작성 없이 실행 시켰다가
+    //메모리주소가 출력되는 문제를 마주하였는데
+    //섹션 강의 듣고 Override로 해결
 }
